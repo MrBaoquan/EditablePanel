be6ca414130ab0a55fe12d5cae0eeafc8c98ff52
MrBaoquan <mrma617@gmail.com>
2019-02-17 23:05:45 +0800

    back editable-screen.

---
+++ Assets/Scenes/CameraRender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnPreRender() {
+     GL.wireframe = true;   
+    }
+    private void OnPostRender() {
+        GL.wireframe = false;
+    }
+}
